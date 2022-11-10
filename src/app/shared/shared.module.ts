429c8ffd0c046221fa65b8c89daed4111a4b52b8
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-11-10 14:35:46 -0600

    creacion de los componentes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FoterComponent } from './foter/foter.component';
+import { NavigateMenuComponent } from './navigate-menu/navigate-menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FoterComponent,
+    NavigateMenuComponent
+  ],
   imports: [
     CommonModule
   ]
